010be47a6f809e148a1356deaea08e24d54ab24b
ziryan <ziryan@bk.ru>
2024-09-17 23:43:31 +0800

    Initial commit

---
+++ src/main/java/pro/sky/c2hw4calculator/C2hw4CalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.c2hw4calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2hw4CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2hw4CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/c2hw4calculator/C2hw4CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.c2hw4calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2hw4CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
